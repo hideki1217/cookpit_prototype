6f21c83077e7f96d691d3c5729d56692c6072b31
HidekiOkumura <okumurahidekikangeki@gmail.com>
2024-03-28 20:33:50 +0900

    Added recipe descripter

--- recipe.py
+++ recipe.py
@@ -1,35 +1,106 @@
-from abc import ABC
-from typing import NewType
 from dataclasses import dataclass
+from collections import defaultdict
 
-    
+
 @dataclass(frozen=True)
 class Node:
     global_name: str
     elements: tuple["Edible"]
     output_names: tuple[str]
-    
+
     @property
     def outputs(self) -> tuple["Edible"]:
         return tuple(Edible(name, self) for name in self.output_names)
-    
-    
+
+    def is_material(self) -> bool:
+        return (len(self.elements) == 0) and (len(self.output_names) == 1)
+
+    def is_operation(self) -> bool:
+        return not self.is_material()
+
+
 @dataclass(frozen=True)
 class Edible:
     local_name: str
     recipe: Node
-    
-    @property
-    def _id(self) -> str:
-        return hash(self)
-    
+
 
 class RecipeBuilder:
     def __init__(self) -> None:
         pass
-    
-    def elem(self, name: str) -> Edible:
+
+    def material(self, name: str) -> Edible:
         return Node(name, tuple(), (name, )).outputs[0]
-    
-    def op(self, name: str, elements: tuple[Edible], outputs: tuple[str]) -> tuple[Edible]:
+
+    def operation(self, name: str, elements: tuple[Edible], outputs: tuple[str]) -> tuple[Edible]:
         return Node(name, elements, outputs).outputs
+
+
+class Descripter:
+    def __init__(self) -> None:
+        pass
+
+    def descript_material(self, edible: Edible) -> str:
+        def find_all_material(node: Node) -> tuple[Node]:
+            if node.is_material():
+                return (node, )
+
+            result = tuple()
+            for elem in node.elements:
+                result += find_all_material(elem.recipe)
+
+            return result
+
+        materials = find_all_material(edible.recipe)
+        mtrls_description = "## 材料\n" + \
+            "\n".join([f"- {elem.global_name}"for elem in materials])
+        return mtrls_description
+
+    def descript_operation(self, edible: Edible) -> str:
+        def topological_sort(node: Node) -> tuple[Node]:
+            # assume: nodeが唯一の出力のないNode
+
+            def refcount_op_dict(node: Node) -> dict[Node, int]:
+                res = defaultdict(int)
+
+                def f(node: Node):
+                    if node.is_material():
+                        return
+
+                    res[node] += 1
+                    for elem in node.elements:
+                        f(elem.recipe)
+
+                f(node)
+                return res
+
+            refcount = refcount_op_dict(node)
+
+            tpl_sorted = []
+            frontier = [node]
+            while len(frontier) != 0:
+                front = frontier.pop()
+                tpl_sorted.append(front)
+
+                for elem in front.elements:
+                    if elem.recipe in refcount:
+                        refcount[elem.recipe] -= 1
+
+                        if refcount[elem.recipe] == 0:
+                            frontier.append(elem.recipe)
+
+            return list(reversed(tpl_sorted))
+
+        index2op = topological_sort(edible.recipe)
+        op2index = {op: index + 1 for index, op in enumerate(index2op)}
+
+        def descript(op: Node):
+            elems = [elem.local_name if elem.recipe.is_material() else str(
+                op2index[elem.recipe]) for elem in op.elements]
+            return f"{'と'.join(elems)}を{op.global_name}"
+
+        op_description = "## 手順\n" + \
+            "\n".join([f"{index+1}. {descript(op)}" for index,
+                      op in enumerate(index2op)])
+
+        return op_description
--- test.py
+++ test.py
@@ -1,24 +1,27 @@
 from recipe import *
-import pprint
+
 
 def main():
     builder = RecipeBuilder()
-    
-    x = builder.elem("牛肉細切れ")
-    y = builder.elem("にんじん")
-    z = builder.elem("じゃがいも")
-    
-    a = builder.elem("しょうゆ")
-    b = builder.elem("砂糖")
-    
-    y_ = builder.op("ざく切り", (y,), (f"ざく切りにしたもの",))
-    z_ = builder.op("ざく切り", (z,), (f"ざく切りにしたもの",))
-    ab_ = builder.op("混ぜ合わせる", (a, b,), (f"混ぜ合わせたもの",))
-    
-    (result,) = builder.op("煮る", (x, *y_, *z_, *ab_), ("肉じゃが",))
-    pprint.pprint(result)
+
+    x = builder.material("牛肉細切れ")
+    y = builder.material("にんじん")
+    z = builder.material("じゃがいも")
+
+    a = builder.material("しょうゆ")
+    b = builder.material("砂糖")
+
+    y_ = builder.operation("ざく切り", (y,), (f"ざく切りにしたもの",))
+    z_ = builder.operation("ざく切り", (z,), (f"ざく切りにしたもの",))
+    ab_ = builder.operation("混ぜ合わせる", (a, b,), (f"混ぜ合わせたもの",))
+
+    recipe = builder.operation("煮る", (x, *y_, *z_, *ab_), ("肉じゃが", "煮汁"))
+
+    descripter = Descripter()
+    print(f"# {recipe[0].local_name} の作り方 \n")
+    print(descripter.descript_material(recipe[0]) + "\n")
+    print(descripter.descript_operation(recipe[0]) + "\n")
 
 
 if __name__ == "__main__":
     main()
-    
