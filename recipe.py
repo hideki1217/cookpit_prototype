cd70fa1d84dd9753dd851961b5ad15579b309652
HidekiOkumura <okumurahidekikangeki@gmail.com>
2024-03-28 22:41:17 +0900

    refactor recipe

--- recipe.py
+++ recipe.py
@@ -1,106 +1,83 @@
 from dataclasses import dataclass
-from collections import defaultdict
+from typing import Literal, Sequence
+from unit import Amount
 
 
-@dataclass(frozen=True)
-class Node:
-    global_name: str
-    elements: tuple["Edible"]
+@dataclass
+class Attribute:
+    _id: int
+    name: str
+    kind: Literal["material", "operation"]
+
+
+@dataclass
+class Material(Attribute):
+    amount: Amount
+
+
+@dataclass
+class Operation(Attribute):
+    pass
+
+
+@dataclass
+class Recipe:
+    detail: Material | Operation
+    inputs: tuple["Edible"]
     output_names: tuple[str]
-
+    
     @property
     def outputs(self) -> tuple["Edible"]:
-        return tuple(Edible(name, self) for name in self.output_names)
+        return tuple(Edible(self, local_name) for local_name in self.output_names)
+    
+    def materials(self) -> tuple[Material]:
+        res = []
 
-    def is_material(self) -> bool:
-        return (len(self.elements) == 0) and (len(self.output_names) == 1)
+        def f(recipe: Recipe):
+            if recipe.detail.kind == "material":
+                res.append(recipe.detail)
 
-    def is_operation(self) -> bool:
-        return not self.is_material()
+            for input in recipe.inputs:
+                f(input.recipe)
+
+        f(self)
+        return tuple(res)
 
 
-@dataclass(frozen=True)
+@dataclass
 class Edible:
+    recipe: Recipe
     local_name: str
-    recipe: Node
 
 
 class RecipeBuilder:
     def __init__(self) -> None:
         pass
 
-    def material(self, name: str) -> Edible:
-        return Node(name, tuple(), (name, )).outputs[0]
+    def material(self, name: str, amount: Amount) -> Edible:
+        # TODO: hard code
+        name2id = {
+            "にんじん": 0,
+            "じゃがいも": 1,
+            "牛肉細切れ": 2,
+            "しょうゆ": 3,
+            "砂糖": 4,
+        }
+        material = Material(name2id[name], name, "material", amount)
+        return Recipe(material, [], (name, )).outputs[0]
 
-    def operation(self, name: str, elements: tuple[Edible], outputs: tuple[str]) -> tuple[Edible]:
-        return Node(name, elements, outputs).outputs
-
-
-class Descripter:
-    def __init__(self) -> None:
-        pass
-
-    def descript_material(self, edible: Edible) -> str:
-        def find_all_material(node: Node) -> tuple[Node]:
-            if node.is_material():
-                return (node, )
-
-            result = tuple()
-            for elem in node.elements:
-                result += find_all_material(elem.recipe)
-
-            return result
-
-        materials = find_all_material(edible.recipe)
-        mtrls_description = "## 材料\n" + \
-            "\n".join([f"- {elem.global_name}"for elem in materials])
-        return mtrls_description
-
-    def descript_operation(self, edible: Edible) -> str:
-        def topological_sort(node: Node) -> tuple[Node]:
-            # assume: nodeが唯一の出力のないNode
-
-            def refcount_op_dict(node: Node) -> dict[Node, int]:
-                res = defaultdict(int)
-
-                def f(node: Node):
-                    if node.is_material():
-                        return
-
-                    res[node] += 1
-                    for elem in node.elements:
-                        f(elem.recipe)
-
-                f(node)
-                return res
-
-            refcount = refcount_op_dict(node)
-
-            tpl_sorted = []
-            frontier = [node]
-            while len(frontier) != 0:
-                front = frontier.pop()
-                tpl_sorted.append(front)
-
-                for elem in front.elements:
-                    if elem.recipe in refcount:
-                        refcount[elem.recipe] -= 1
-
-                        if refcount[elem.recipe] == 0:
-                            frontier.append(elem.recipe)
-
-            return list(reversed(tpl_sorted))
-
-        index2op = topological_sort(edible.recipe)
-        op2index = {op: index + 1 for index, op in enumerate(index2op)}
-
-        def descript(op: Node):
-            elems = [elem.local_name if elem.recipe.is_material() else str(
-                op2index[elem.recipe]) for elem in op.elements]
-            return f"{'と'.join(elems)}を{op.global_name}"
-
-        op_description = "## 手順\n" + \
-            "\n".join([f"{index+1}. {descript(op)}" for index,
-                      op in enumerate(index2op)])
-
-        return op_description
+    def operation(self, name: str, inputs: Sequence[Edible], outputs: Sequence[str]) -> tuple[Edible]:
+        # TODO: hard code
+        name2id = {
+            "蒸す": 0,
+            "焼く": 1,
+            "炒める": 2,
+            "茹でる": 3,
+            "混ぜる": 4,
+            "ざく切りにする": 5,
+            "混ぜ合わせる": 6,
+            "煮る": 7,
+        }
+        op = Operation(name2id[name], name, 'operation')
+        return Recipe(op, tuple(inputs), tuple(outputs)).outputs
+    
--- test.py
+++ test.py
@@ -1,26 +1,23 @@
+from pprint import pprint
 from recipe import *
-
+from unit import Weight, Count, Volume
 
 def main():
     builder = RecipeBuilder()
 
-    x = builder.material("牛肉細切れ")
-    y = builder.material("にんじん")
-    z = builder.material("じゃがいも")
+    x = builder.material("牛肉細切れ", Weight(120))
+    y = builder.material("にんじん", Count(1))
+    z = builder.material("じゃがいも", Count(3))
 
-    a = builder.material("しょうゆ")
-    b = builder.material("砂糖")
+    a = builder.material("しょうゆ", Volume(6))
+    b = builder.material("砂糖", Volume(6))
 
-    y_ = builder.operation("ざく切り", (y,), (f"ざく切りにしたもの",))
-    z_ = builder.operation("ざく切り", (z,), (f"ざく切りにしたもの",))
+    y_ = builder.operation("ざく切りにする", (y,), (f"ざく切りにしたもの",))
+    z_ = builder.operation("ざく切りにする", (z,), (f"ざく切りにしたもの",))
     ab_ = builder.operation("混ぜ合わせる", (a, b,), (f"混ぜ合わせたもの",))
 
     recipe = builder.operation("煮る", (x, *y_, *z_, *ab_), ("肉じゃが", "煮汁"))
-
-    descripter = Descripter()
-    print(f"# {recipe[0].local_name} の作り方 \n")
-    print(descripter.descript_material(recipe[0]) + "\n")
-    print(descripter.descript_operation(recipe[0]) + "\n")
+    pprint(recipe)
 
 
 if __name__ == "__main__":
